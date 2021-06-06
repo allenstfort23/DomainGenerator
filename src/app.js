3fbaa29a036b23dac3b8aab391708130fb0b0857
Allen St Fort <allenstfort1@gmail.com>
2021-06-06 18:09:52 +0000

    This is the Domain Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,33 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    let randomPronoun = Math.floor(Math.random() * pronoun.length);
+    //  console.log(pronoun[randomPronoun]);
+    for (let j = 0; j < adj.length; j++) {
+      let randomAdj = Math.floor(Math.random() * adj.length);
+
+      for (let x = 0; x < noun.length; x++) {
+        let randomNoun = Math.floor(Math.random() * noun.length);
+
+        for (let z = 0; z < extensions.length; z++) {
+          let randomExtensions = Math.floor(Math.random() * extensions.length);
+
+          console.log(
+            pronoun[randomPronoun] +
+              adj[randomAdj] +
+              noun[randomNoun] +
+              extensions[randomExtensions]
+          );
+        }
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
